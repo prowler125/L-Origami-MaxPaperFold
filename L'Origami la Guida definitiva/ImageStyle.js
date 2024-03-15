f16bb6d30288a6e9c770be8c1f0733e6b971e2ba
Prowler <massimiliamomancini@gmail.com>
2024-03-15 10:27:59 +0100

    Refactor modal functionality and update image IDs

--- L'Origami la Guida definitiva/ImageStyle.js
+++ L'Origami la Guida definitiva/ImageStyle.js
@@ -1,14 +1,22 @@
-var modal= document.getElementById('PeriodoEdo');
-var img=
-document.getElementById('PeriodoEdo');
-var modalImg = document.getElementById("PeriodoEdo");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-    }
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
+function openModal() {
+    document.getElementById("Edo").style.display = "block";
 }
+
+function closeModal() {
+    document.getElementById("Edo").style.display = "none";
+}
+document.addEventListener("DOMContentLoaded", function() {
+    var modal = document.getElementById("Edo");
+    var images = document.querySelectorAll(".modal-image");
+    var closeBtn = document.querySelector(".close");
+
+    images.forEach(function(_) {
+        _.addEventListener("click", function() {
+            modal.style.display = "block";
+        });
+    });
+
+    closeBtn.addEventListener("click", function() {
+        modal.style.display = "none";
+    });
+});
